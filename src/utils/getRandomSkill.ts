f49b144b11a6f5f2e8007905e6471fddaa42c5df
Evgeny <seedofpanic@mail.ru>
2018-12-04 21:38:26 +0000

    fix crush on player exit

--- src/utils/getRandomSkill.ts
+++ src/utils/getRandomSkill.ts
@@ -2,6 +2,10 @@ import {fairRandom} from './fairRandom';
 import {IUnit} from '../models/unit';
 
 export function getRandomSkill(player: IUnit) {
+    if (!player.character) {
+        return '';
+    }
+
     const actionsKeys = Object.keys(player.character.actions)
         .filter(actionId => {
             return player.character.actions[actionId].isAvailable();
